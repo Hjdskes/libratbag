005467cefa5bb70a879188d0e5f561d90962aecf
Peter Hutterer <peter.hutterer@who-t.net>
2017-08-30 09:30:04 +1000

    Make success a part of the logger class
    It's still a strange setup because we check the logger class for errors, not
    the actual parser bits but let's ignore that for now. Let's be happy that we
    don't have to track success as separate variable.
    
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

--- data/gnome/check-svg.py
+++ data/gnome/check-svg.py
@@ -8,9 +8,22 @@ import logging
 ns = {'svg': 'http://www.w3.org/2000/svg'}
 style_query = '//svg:rect[@id=\"{}\"][contains(@style, \"{}\")]'
 
-logging.basicConfig(level=logging.DEBUG)
 logger = None
-success = True
+
+class SVGLogger(logging.Logger):
+    def __init__(self, name, level=logging.NOTSET):
+        self.success = True
+        return super().__init__(name, level)
+
+    def error(self, msg, *args, **kwargs):
+        self.success = False
+        return super().error(msg, *args, **kwargs)
+
+    @classmethod
+    def get_logger(cls, path):
+        logging.setLoggerClass(SVGLogger)
+        logging.basicConfig(level=logging.DEBUG)
+        return logging.getLogger(path)
 
 
 def check_size(root):
@@ -26,14 +39,11 @@ def check_layers(root):
     """
     Check there are layers (well, groups) for the components we require.
     """
-    global success
-
     layer_ids = [g.attrib['id'] for g in root.iterfind('svg:g', ns)]
 
     for layer in ["Device", "Buttons", "LEDs"]:
         if layer not in layer_ids:
             logger.error("Missing layer: {}".format(layer))
-            success = False
 
 
 def check_elements(root, prefix, required=0):
@@ -45,8 +55,6 @@ def check_elements(root, prefix, required=0):
     If required is nonzero, an error is logged for any missing element with
     an index less than required.
     """
-
-    global success
 
     # elements can be paths and rects
     # This includes leaders and lines
@@ -63,25 +71,20 @@ def check_elements(root, prefix, required=0):
             previous = '{}{}'.format(prefix, idx - 1)
             if idx > 0 and previous not in element_ids:
                 logger.error("Non-consecutive {}: {}".format(prefix, e))
-                success = False
 
             leader = '{}{}-leader'.format(prefix, idx)
             if leader not in element_ids:
                 logger.error("Missing {} for {}".format(leader, e))
-                success = False
             else:
                 element = root.xpath(style_query.format(leader, 'text-align'), namespaces=ns)
                 if element is None or len(element) != 1 or element[0] is None:
                     logger.error("Missing style property for {}".format(leader))
-                    success = False
 
             path = '{}{}-path'.format(prefix, idx)
             if path not in element_ids:
                 logger.error("Missing {} for {}".format(path, e))
-                success = False
         elif idx < required:
             logger.error("Missing {}: {}".format(prefix, e))
-            success = False
 
     logger.info("Found {} {}s".format(highest + 1, prefix))
 
@@ -104,9 +107,12 @@ def check_svg(path):
     check_leds(root)
 
 if __name__ == "__main__":
+    success = True
     for path in sys.argv[1:]:
-        logger = logging.getLogger(path)
+        logger = SVGLogger.get_logger(path)
         check_svg(path)
+        if not logger.success:
+            success = False
 
     if not success:
         sys.exit(1)
